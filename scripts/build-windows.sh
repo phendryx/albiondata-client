da0561c3d0fe75181c57cc35ad5e746b47ff3c7c
Paul Hendryx <phendryx@gmail.com>
2022-12-27 16:17:23 -0500

    testing workflows

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -2,6 +2,10 @@
 
 set -eo pipefail
 
+GITHUB_REF_NAME=1.2.3
+
+sudo apt update && sudo apt install -y nsis nsis-pluginapi
+
 export CGO_CPPFLAGS="-I $GOPATH/src/github.com/broderickhyman/albiondata-client/thirdparty/WpdPack/Include/"
 export CGO_LDFLAGS="-L $GOPATH/src/github.com/broderickhyman/albiondata-client/thirdparty/WpdPack/Lib/x64/"
 export GOOS=windows
@@ -9,7 +13,7 @@ export GOARCH=amd64
 export CGO_ENABLED=1
 export CXX=x86_64-w64-mingw32-g++
 export CC=x86_64-w64-mingw32-gcc
-go build -ldflags "-s -w -X main.version=$CIRCLE_TAG" -o albiondata-client.exe -v -x albiondata-client.go
+go build -ldflags "-s -w -X main.version=$GITHUB_REF_NAME" -o albiondata-client.exe -v -x albiondata-client.go
 
 # Add icon to the .exe
 wine thirdparty/rcedit/rcedit.exe albiondata-client.exe --set-icon icon/albiondata-client.ico
