6ef68b72afa7295699aaf62bfed6681e1df9fb8c
Paul Hendryx <phendryx@gmail.com>
2022-12-27 20:28:12 -0500

    lets go darwin?

--- scripts/build-darwin.sh
+++ scripts/build-darwin.sh
@@ -2,64 +2,7 @@
 
 set -eo pipefail
 
-# apt-get update && sudo apt-get install -y libpcap-dev zip
-
-sudo dpkg --add-architecture arm64                      \
- && sudo dpkg --add-architecture armel                      \
- && sudo dpkg --add-architecture armhf                      \
- && sudo dpkg --add-architecture i386                       \
- && sudo dpkg --add-architecture mips                       \
- && sudo dpkg --add-architecture mipsel                     \
- && sudo dpkg --add-architecture powerpc                    \
- && sudo dpkg --add-architecture ppc64el                    \
- && sudo apt-get update                                     \
- && sudo apt-get install -y -q                              \
-        autoconf                                       \
-        automake                                       \
-        autotools-dev                                  \
-        bc                                             \
-        binfmt-support                                 \
-        binutils-multiarch                             \
-        binutils-multiarch-dev                         \
-        build-essential                                \
-        clang                                          \
-        crossbuild-essential-arm64                     \
-        crossbuild-essential-armel                     \
-        crossbuild-essential-armhf                     \
-        crossbuild-essential-mipsel                    \
-        crossbuild-essential-ppc64el                   \
-        curl                                           \
-        devscripts                                     \
-        gdb                                            \
-        git-core                                       \
-        libtool                                        \
-        llvm                                           \
-        mercurial                                      \
-        multistrap                                     \
-        patch                                          \
-        software-properties-common                     \
-        subversion                                     \
-        wget                                           \
-        xz-utils                                       \
-        cmake                                          \
-        qemu-user-static                               \
-        libxml2-dev                                    \
-        lzma-dev                                       \
-        openssl                                        \
-        libssl-dev                                     \
-
-sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-		g++ \
-		gcc \
-		libc6-dev \
-		make \
-		pkg-config \
-        ca-certificates \
-        wget \  
-        git \      
-	build-essential \
-	mingw-w64 \
-	nsis
+apt-get update && apt-get install -y libpcap-dev zip
 
 export OSXCROSS_NO_INCLUDE_PATH_WARNINGS=1
 export MACOSX_DEPLOYMENT_TARGET=10.6
@@ -87,7 +30,6 @@ cp -v run.command ./$TEMP/run.command
 chown -Rv ${USER}:${USER} ./$TEMP
 chmod -v 777 ./$TEMP/*
 zip -v ../$ZIPNAME -r ./"$TEMP"
-ls -la
 
 # In theory the following works to create an app but there was a permissions issue when opening on the mac
 # APP_NAME="Albion Data Client"
