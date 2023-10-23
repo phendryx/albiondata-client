818a1836a272a0d8d4c82d6be659ce2d36a4d614
Daniel Koch <koch@triple6.org>
2023-10-23 19:52:46 +0200

    PCAP: Add interface filter for GNU/Linux

---
+++ client/net_interface_filter_darw.go
@@ -0,0 +1,25 @@
+// +build darwin
+
+package client
+
+import (
+	"net"
+)
+
+// Gets all physical interfaces based on filter results, ignoring all VM, Loopback and Tunnel interfaces.
+func getAllPhysicalInterface() ([]string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	var outInterfaces []string
+
+	for _, _interface := range interfaces {
+		if _interface.Flags&net.FlagLoopback == 0 && _interface.Flags&net.FlagUp == 1 && isPhysicalInterface(_interface.HardwareAddr.String()) {
+			outInterfaces = append(outInterfaces, _interface.Name)
+		}
+	}
+
+	return outInterfaces, nil
+}
